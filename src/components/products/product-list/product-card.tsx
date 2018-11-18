53dc734eddfc62c9dd27a6126536c2f767e9a39c
bishoybishai <bishoy.bishai@gmail.com>
2018-11-18 14:38:04 +0200

    add style to product page

--- src/components/products/product-list/product-card.tsx
+++ src/components/products/product-list/product-card.tsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 import { PATHS } from "../../../router/routes";
 import { IProduct } from "../product-modal";
 import { geCalenderDate } from "../../../helper/date";
-
+import "./style.scss";
 
 const ProductCard = ({ product }: { product: IProduct }) => {
   return (
-    <Card>
+    <Card className="product-card">
       <Card.Content>
         <Image floated="right" size="mini" src={product.image} />
         <Card.Header>{product.title}</Card.Header>
--- src/components/products/product/product.tsx
+++ src/components/products/product/product.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Icon, Item } from "semantic-ui-react";
+import { Button, Icon, Item, Card, Image } from "semantic-ui-react";
 import { IProduct } from "../product-modal";
 import PageLoader from "../../layout/loader/loader";
 import { geCalenderDate } from "../../../helper/date";
@@ -36,48 +36,40 @@ class Product extends React.Component<{
     return (
       <div>
         {product ? (
-          <Item.Group className="product-card">
-            <Item>
-              <Item.Image src={product.image} />
+          <Card fluid className="product-details">
+            <Image size="medium" centered src={product.image} />
+            <Card.Header>{product.title}</Card.Header>
+            <Card.Meta>{geCalenderDate(product.date_created)}</Card.Meta>
+            <Card.Meta>{product.price}</Card.Meta>
+            <Card.Description>{product.description}</Card.Description>
 
-              <Item.Content>
-                <Item.Header as="a">{product.title}</Item.Header>
-                <Item.Meta>
-                  <span className="cinema">
-                    {geCalenderDate(product.date_created)}
-                  </span>
-                </Item.Meta>
-                <Item.Meta>
-                  <span className="cinema">{product.price} $</span>
-                </Item.Meta>
-                <Item.Description>{product.description}</Item.Description>
-                {currentUser === product.uid && (
-                  <Item.Extra>
-                    <Button
-                      onClick={() => this.props.edit(product.id)}
-                      icon
-                      labelPosition="left"
-                      positive
-                      floated="right"
-                    >
-                      <Icon name="edit" />
-                      Edit
-                    </Button>
-                    <Button
-                      onClick={this.openDeleteModal}
-                      icon
-                      labelPosition="right"
-                      negative
-                      floated="right"
-                    >
-                      <Icon name="delete" />
-                      Delete
-                    </Button>
-                  </Item.Extra>
-                )}
-              </Item.Content>
-            </Item>
-          </Item.Group>
+            {currentUser === product.uid && (
+              <Card.Content textAlign="center" extra>
+                <div>
+                  <Button
+                    onClick={() => this.props.edit(product.id)}
+                    icon
+                    labelPosition="left"
+                    positive
+                    floated="right"
+                  >
+                    <Icon name="edit" />
+                    Edit
+                  </Button>
+                  <Button
+                    onClick={this.openDeleteModal}
+                    icon
+                    labelPosition="right"
+                    negative
+                    floated="right"
+                  >
+                    <Icon name="delete" />
+                    Delete
+                  </Button>
+                </div>
+              </Card.Content>
+            )}
+          </Card>
         ) : (
           <PageLoader />
         )}
